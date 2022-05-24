fbe73d0ffc78737cdd0ba0bb45d9cf76f568875b
Jayash Satolia <jayash.s@goglocal.live>
2022-05-24 12:37:00 +0530

    Switched database to postgresql from sqlite

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-import sqlite3
-import os
+import psycopg2
+
 import pandas as pd
 
-# if test.db exists, delete it
-if os.path.exists("test.db"):
-    os.remove("test.db")
-
-connection = sqlite3.connect("test.db")
-cursor = connection.cursor()
+conn = psycopg2.connect("dbname=logistics user=postgres password=postgres")
+conn.autocommit = True
+cursor = conn.cursor()
 
 
 
 cursor.execute(
-    "CREATE TABLE chapters (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL)")
+    "CREATE TABLE chapters (id BIGSERIAL PRIMARY KEY NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL)")
 cursor.execute(
-    "CREATE TABLE headings (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL, chapter TEXT NOT NULL, FOREIGN KEY (chapter) REFERENCES chapters (id))")
+    "CREATE TABLE headings (id BIGSERIAL PRIMARY KEY NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL, chapter BIGSERIAL REFERENCES chapters(id) ON DELETE CASCADE)")
 cursor.execute(
-    "CREATE TABLE subheadings (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL, heading TEXT NOT NULL, FOREIGN KEY (heading) REFERENCES headings (id))")
+    "CREATE TABLE subheadings (id BIGSERIAL PRIMARY KEY NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL, heading BIGSERIAL REFERENCES headings (id) ON DELETE CASCADE)")
 cursor.execute(
-    "CREATE TABLE country_wise (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL, country TEXT NOT NULL, subheading TEXT NOT NULL, FOREIGN KEY (subheading) REFERENCES subheadings (id))")
+    "CREATE TABLE country_wise (id BIGSERIAL PRIMARY KEY NOT NULL, description TEXT NOT NULL, code TEXT NOT NULL, country TEXT NOT NULL, subheading BIGSERIAL REFERENCES subheadings (id) ON DELETE CASCADE)")
 cursor.execute(
-    "CREATE TABLE custom_duty (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, hscode TEXT NOT NULL, country TEXT NOT NULL)")
+    "CREATE TABLE custom_duty (id BIGSERIAL PRIMARY KEY NOT NULL, hscode TEXT NOT NULL, country TEXT NOT NULL)")
 cursor.execute(
-    "CREATE TABLE sales_tax (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, hscode TEXT NOT NULL, country TEXT NOT NULL)")
+    "CREATE TABLE sales_tax (id BIGSERIAL PRIMARY KEY NOT NULL, hscode TEXT NOT NULL, country TEXT NOT NULL)")
 cursor.execute(
-    "CREATE TABLE vat (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, hscode TEXT NOT NULL, country TEXT NOT NULL)")
+    "CREATE TABLE vat (id BIGSERIAL PRIMARY KEY NOT NULL, hscode TEXT NOT NULL, country TEXT NOT NULL)")
 
 
 
@@ -38,9 +35,9 @@ country_wise = pd.read_csv("country_wise.csv")
 for rows in chapters.itertuples():
     code, description = rows[1], rows[2]
     code, description = str(code), str(description)
-    cursor.execute("INSERT INTO chapters (description, code) VALUES (?, ?)", (description, code))
+    cursor.execute("INSERT INTO chapters (description, code) VALUES (%s, %s)", (description, code))
 
-connection.commit()
+conn.commit()
 
 
 for rows in headings.itertuples():
@@ -48,12 +45,12 @@ for rows in headings.itertuples():
     code, description = str(code), str(description)
     
     chapter_code = code[:2]
-    cursor.execute("SELECT id FROM chapters WHERE code = ?", (chapter_code,))
+    cursor.execute("SELECT id FROM chapters WHERE code = %s", (chapter_code,))
     chapter_id = cursor.fetchone()[0]
 
-    cursor.execute("INSERT INTO headings (description, code, chapter) VALUES (?, ?, ?)", (description, code, chapter_id))
+    cursor.execute("INSERT INTO headings (description, code, chapter) VALUES (%s, %s, %s)", (description, code, chapter_id))
 
-connection.commit()
+conn.commit()
 
 
 for rows in subheadings.itertuples():
@@ -61,12 +58,12 @@ for rows in subheadings.itertuples():
     code, description = str(code), str(description)
 
     heading_code = code[:4]
-    cursor.execute("SELECT id FROM headings WHERE code = ?", (heading_code,))
+    cursor.execute("SELECT id FROM headings WHERE code = %s", (heading_code,))
     heading_id = cursor.fetchone()[0]
 
-    cursor.execute("INSERT INTO subheadings (description, code, heading) VALUES (?, ?, ?)", (description, code, heading_id))
+    cursor.execute("INSERT INTO subheadings (description, code, heading) VALUES (%s, %s, %s)", (description, code, heading_id))
 
-connection.commit()
+conn.commit()
 
 
 for rows in country_wise.itertuples():
@@ -74,10 +71,10 @@ for rows in country_wise.itertuples():
     code, description = str(code), str(description)
 
     subheading_code = code[:6]
-    cursor.execute("SELECT id FROM subheadings WHERE code = ?", (subheading_code,))
+    cursor.execute("SELECT id FROM subheadings WHERE code = %s", (subheading_code,))
     subheading_id = cursor.fetchone()[0]
 
-    cursor.execute("INSERT INTO country_wise (description, code, country, subheading) VALUES (?, ?, ?, ?)", (description, code, "USA", subheading_id))
+    cursor.execute("INSERT INTO country_wise (description, code, country, subheading) VALUES (%s, %s, %s, %s)", (description, code, "USA", subheading_id))
 
 
-connection.commit()
+conn.commit()
